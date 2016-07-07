dd5c52f819477730f5e0f03333f210664d9bd60f
eswat2 <eswat2@gmail.com>
2016-07-07 10:09:03 -0700

    lesson 6 - propTypes

--- app/components/Github/UserProfile.jsx
+++ app/components/Github/UserProfile.jsx
@@ -12,4 +12,9 @@ class UserProfile extends React.Component {
   }
 }
 
+UserProfile.propTypes = {
+ username: React.PropTypes.string.isRequired,
+ bios: React.PropTypes.object.isRequired
+};
+
 export default UserProfile;
--- app/components/Notes/Notes.jsx
+++ app/components/Notes/Notes.jsx
@@ -3,7 +3,6 @@ import NotesList from './NotesList.jsx';
 
 class Notes extends React.Component {
   render() {
-    console.log(`-- Notes:  ${this.props.notes}`)
     return (
       <div>
         <h3>Notes for {this.props.username} </h3>
@@ -12,5 +11,10 @@ class Notes extends React.Component {
     )
   }
 }
+
+Notes.propTypes = {
+ username: React.PropTypes.string.isRequired,
+ notes: React.PropTypes.array.isRequired
+};
 
 export default Notes;
