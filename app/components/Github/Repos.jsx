6b2d102e6ea05e4ed562062ef8718abc5120cf6f
eswat2 <eswat2@gmail.com>
2016-07-07 16:33:53 -0700

    lesson 14 - more refactoring

--- app/components/Github/Repos.jsx
+++ app/components/Github/Repos.jsx
@@ -2,19 +2,17 @@ import React from 'react';
 
 class Repos extends React.Component {
   render() {
-    let repos = this.props.repos.map(function(repo,index){
-      return (
-        <li className="list-group-item" key={index}>
-          {repo.html_url && <h4><a href={repo.html_url}>{repo.name}</a></h4>}
-          {repo.description && <p>{repo.description}</p>}
-        </li>
-      );
-    })
+    const { repos } = this.props;
     return (
       <div>
         <h3>User Repos</h3>
         <ul className="list-group">
-          {repos}
+          {repos.map((repo,index) => (
+              <li className="list-group-item" key={index}>
+                {repo.html_url && <h4><a href={repo.html_url}>{repo.name}</a></h4>}
+                {repo.description && <p>{repo.description}</p>}
+              </li>
+            ))}
         </ul>
       </div>
     )
