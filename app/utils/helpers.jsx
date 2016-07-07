a097b1bf1512df6ae3d87539235100bb079dca6d
eswat2 <eswat2@gmail.com>
2016-07-07 15:59:34 -0700

    lession 12 - refactor for es6
    -- nothing much changed since i was already doing the es6 things

--- app/utils/helpers.jsx
+++ app/utils/helpers.jsx
@@ -8,17 +8,7 @@ function getUserInfo(username){
   return axios.get(`https://api.github.com/users/${username}`);
 }
 
-const helpers = {
-  getGithubInfo: (username) => {
-    return axios.all([getRepos(username), getUserInfo(username)])
-      .then(function(arr){
-        console.log(arr);
-        return {
-          repos: arr[0].data,
-          bios: arr[1].data
-        }
-      });
-  }
+export default function getGithubInfo(username) {
+  return axios.all([getRepos(username), getUserInfo(username)])
+    .then((arr) => ({repos: arr[0].data, bios: arr[1].data}));
 }
-
-export default helpers;
