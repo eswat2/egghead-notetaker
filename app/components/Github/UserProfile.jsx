04206a6038fb2440cfe0edb550848b44b6372a4f
eswat2 <eswat2@gmail.com>
2016-07-07 17:01:04 -0700

    lesson 15 - more cleanup

--- app/components/Github/UserProfile.jsx
+++ app/components/Github/UserProfile.jsx
@@ -11,9 +11,9 @@ class UserProfile extends React.Component {
         {this.props.bios.email && <li className="list-group-item">Email: {this.props.bios.email}</li>}
         {this.props.bios.location && <li className="list-group-item">Location: {this.props.bios.location}</li>}
         {this.props.bios.company && <li className="list-group-item">Company: {this.props.bios.company}</li>}
-        {this.props.bios.followers && <li className="list-group-item">Followers: {this.props.bios.followers}</li>}
-        {this.props.bios.following && <li className="list-group-item">Following: {this.props.bios.following}</li>}
-        {this.props.bios.public_repos && <li className="list-group-item">Public Repos: {this.props.bios.public_repos}</li>}
+        {this.props.bios.followers != 0 && <li className="list-group-item">Followers: {this.props.bios.followers}</li>}
+        {this.props.bios.following != 0 && <li className="list-group-item">Following: {this.props.bios.following}</li>}
+        {this.props.bios.public_repos != 0 && <li className="list-group-item">Public Repos: {this.props.bios.public_repos}</li>}
         {this.props.bios.blog && <li className="list-group-item">Blog: <a href={this.props.bios.blog}> {this.props.bios.blog}</a></li>}
       </div>
     )
