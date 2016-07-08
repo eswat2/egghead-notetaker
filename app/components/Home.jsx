080345abb51cb164d3e752ba0569e0e5c1db9e80
eswat2 <eswat2@gmail.com>
2016-07-07 20:22:30 -0700

    lesson 17 - refactor for stateless components

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
-class Home extends React.Component{
-  render(){
-    return(
-      <h2 className="text-center">
-        Search By Github Username Above <i className="fa fa-ellipsis-h"></i> <i className="fa fa-search"></i> <i className="fa fa-github"></i>
-      </h2>
-    )
-  }
-};
+const Home = () => {
+  return (
+    <h2 className="text-center">
+      Search By Github Username Above <i className="fa fa-ellipsis-h"></i> <i className="fa fa-search"></i> <i className="fa fa-github"></i>
+    </h2>
+  )
+}
 
 export default Home;
